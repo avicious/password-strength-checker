518624c97e7bf0d86763122d499e17853eb1c19a
Shubham Kumar <shubham.kr2951@gmail.com>
2026-02-21 23:31:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import { Eye, EyeClosed } from "lucide-react";
+
+const App = () => {
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [progress, setProgress] = useState("");
+  const [hidePassword, setHidePassword] = useState(true);
+
+  const getActiveColor = (type) => {
+    if (type === "Strong") return "#3fbb60";
+    if (type === "Medium") return "#fe804d";
+    return "#ff0054";
+  };
+
+  const handlePassword = (passwordValue) => {
+    const strengthChecks = {
+      length: 0,
+      hasUpperCase: false,
+      hasLowerCase: false,
+      hasNumbers: false,
+      hasSpecialChar: false,
+    };
+
+    strengthChecks.length = passwordValue.length >= 8 ? true : false;
+    strengthChecks.hasUpperCase = /[A-Z]+/.test(passwordValue);
+    strengthChecks.hasLowerCase = /[a-z]+/.test(passwordValue);
+    strengthChecks.hasNumbers = /[0-9]+/.test(passwordValue);
+    strengthChecks.hasSpecialChar = /[^A-Za-z0-9]+/.test(passwordValue);
+
+    let verifiedList = Object.values(strengthChecks).filter((value) => value);
+
+    let strength =
+      verifiedList.length === 5
+        ? "Strong"
+        : verifiedList.length >= 2
+          ? "Medium"
+          : "Weak";
+
+    setPassword(passwordValue);
+    setProgress(`${(verifiedList.length / 5) * 100}%`);
+    setMessage(strength);
+  };
+
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-header">
+          <h1 className="title">Password Strength Checker</h1>
+        </div>
+
+        <div className="card-body">
+          <div className="input-container">
+            <div className="input-box">
+              <input
+                value={password}
+                onChange={({ target }) => {
+                  handlePassword(target.value);
+                }}
+                type={hidePassword ? "password" : "text"}
+                className="input"
+                placeholder="Enter Password"
+              />
+
+              <a
+                href="#"
+                className="toggle-btn"
+                onClick={() => setHidePassword((prev) => !prev)}
+              >
+                {hidePassword ? (
+                  <EyeClosed color="#94a3b8" size={20} />
+                ) : (
+                  <Eye color="#2563eb" size={20} />
+                )}
+              </a>
+            </div>
+
+            <div className="progress-bg">
+              <div
+                className="progress"
+                style={{
+                  width: progress,
+                  backgroundColor: getActiveColor(message),
+                }}
+              ></div>
+            </div>
+          </div>
+
+          {password.length !== 0 ? (
+            <p className="message" style={{ color: getActiveColor(message) }}>
+              Your password is : {message}
+            </p>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
